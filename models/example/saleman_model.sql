85f7a7d91ff8e4e3bed48b0af11040650e4531fe
thang.truong@base.vn <thang.truong@base.vn>
2023-03-21 11:51:36 +0000

    commit

--- models/example/saleman_model.sql
+++ models/example/saleman_model.sql
@@ -1,11 +1,4 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
 {{ config(materialized="table") }}
 
 with
@@ -27,8 +20,4 @@ with
     )
 
 select *
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
--- where id is not null
 from source_data
