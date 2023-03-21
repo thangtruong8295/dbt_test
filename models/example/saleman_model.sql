afaff44d8103b32310a2e8a4ce8e208c2a3c544b
thang.truong@base.vn <thang.truong@base.vn>
2023-03-21 11:45:07 +0000

    commit

--- models/example/saleman_model.sql
+++ models/example/saleman_model.sql
@@ -6,21 +6,29 @@
 
     Try changing "table" to "view" below
 */
+{{ config(materialized="table") }}
 
-{{ config(materialized='table') }}
+with
+    source_data as (
 
-with source_data as (
+        select
+            user_id,
+            email,
+            code_name,
+            system_id,
+            concat(last_name, ' ', first_name) as full_name,
+            title,
+            status,
+            role,
+            date_of_birth
+        from `base-datateam.db_system.user_info`
+        where metatype <> 'guest'
+        order by system_id asc, user_id asc
+    )
 
-SELECT * FROM base-datateam.base_dbo.salesman
-
-)
-
-select id, pipedriveid, email, name, location, status
-from source_data
-where email <> ''
-order by id desc
+select *
 /*
     Uncomment the line below to remove records with null `id` values
 */
-
 -- where id is not null
+from source_data
